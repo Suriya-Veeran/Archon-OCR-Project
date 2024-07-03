b3c49b562fadbf5f0a4bb4565c9853de23c48e36
Suriya-Veeran <suriya.veeran@platform3solutions.com>
2024-07-03 19:41:27 +0530

    Paddle ocr code

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -1,3 +1,31 @@
 
 J
 Document-Scanner/reader.py,f/a/fa77472af365c7326d594eca1703ec755b316c16
+P
+ Document-Scanner/ReaderWithCV.py,0/5/055ded21c9d4bb6eaab230e1ea25e1d8a9a3e509
+V
+&Document-Scanner/Kraken/Kraken_Scan.py,3/4/34ef4d9ea18e113915b34c591f543f0953efd6e3
+Y
+)Document-Scanner/Kraken/Kraken-Printed.py,b/9/b9b1657f51ad16a23d8a5dc0249aff2f86378117
+L
+Document-Scanner/suryaocr.py,e/9/e90c3233c6210d62f2f7051bbfecc50aa1dac1ce
+J
+Document-Scanner/paddle.py,b/0/b085d5894031e6ae94b6479168e3ee08fe4b0fc2
+O
+Document-Scanner/paddle-test.py,7/8/78f660793d0d6680a1828424207d36accde9bbf5
+@
+requirements.txt,1/9/19359a61ae2446b51b549167b014da2fcf265768
+L
+Document-Scanner/__init__.py,5/3/53e6d368c76266e8e69d1db7265483bbd687ef4d
+c
+3Document-Scanner/retrieve_data_using_coordinates.py,3/6/36ed2b314e5dc35cea268f0e062934f97b622613
+O
+Document-Scanner/coordinates.py,c/6/c689e7ab5a5072fd1ca705aa512a17d0e9491d53
+O
+Document-Scanner/OCR-Scanner.py,4/f/4f4d02c0bc7049133a7368d533daa7f4280de88e
+N
+Document-Scanner/ocr_reader.py,1/f/1fcce2c5b174fba937decf8d022cef706263efc0
+S
+#.idea/sonarlint/issuestore/index.pb,9/f/9fe84ebb15faf917b7def6236dba604453cc61e0
+]
+-.idea/sonarlint/securityhotspotstore/index.pb,8/8/88a9255124c95bdc913197c120a8d560edc59c8e
---
+++ Document-Scanner/ocr_reader.py
@@ -0,0 +1,49 @@
+import cv2
+from pdf2image import convert_from_path
+import numpy as np
+from paddleocr import PaddleOCR
+
+pdf_path = r"/home/p3/IdeaProjects/OCR-Project/DepositSlip/Adobe Scan 2 Jul 2024-1.pdf"
+
+pdf_pages = convert_from_path(pdf_path)
+
+width_position = 750
+height_position = 120
+top_position = 320
+left_position = 100
+
+
+def is_bounding_box_within(bounding_box, position):
+    x1, y1 = bounding_box[0]
+    x2, y2 = bounding_box[3]
+
+    pos_x = position['x']
+    pos_y = position['y']
+    pos_width = position['width']
+    pos_height = position['height']
+
+    return (x1 >= pos_x and x2 <= pos_x + pos_width and
+            y1 >= pos_y and y2 <= pos_y + pos_height)
+
+
+def fetch_value_using_coordinates(img, top, left, width, height):
+    thresh = 255 - cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+    ocr = PaddleOCR(use_angle_cls=True, lang='en', det_lang='ml')
+    result = ocr.ocr(thresh, cls=True)
+    bb = is_bounding_box_within(result[0][0][0], {'x': top, 'y': left, 'width': width, 'height': height})
+    print("flag", bb)
+    if bb:
+        roi = thresh[top:top + height, left:left + width]
+        res = ocr.ocr(roi, cls=True)
+        print("res", res)
+
+
+def process_page(page):
+    img = cv2.cvtColor(np.array(page), cv2.COLOR_BGR2GRAY)
+    fetch_value_using_coordinates(img, top_position, left_position, width_position, height_position)
+
+
+# Process each page
+for pages in pdf_pages:
+    process_page(pages)
